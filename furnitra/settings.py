b64236232a4690616fe1fcb85a8e35a1f973b6f1
Ruifeng Li <tamagorchee@gmail.com>
2021-01-20 15:47:55 -0800

    typo

--- furnitra/settings.py
+++ furnitra/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoice.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'furnitra.urls'
