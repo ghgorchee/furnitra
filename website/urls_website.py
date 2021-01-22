bce90357ac15c923276582a498d53f8acfb5bf23
Ruifeng Li <tamagorchee@gmail.com>
2021-01-21 19:56:10 -0800

    progress update

--- website/urls_website.py
+++ website/urls_website.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-]
+    path('blueprint', views.blueprint, name="blueprint"),
+    path('calc', views.calc, name="calc"),
+    path('mystyle', views.mystyle, name="mystyle"),
+]	
--- website/views.py
+++ website/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+
 def home(request):
 	return render(request, 'home.html', {})
+
+
+def blueprint(request):
+	return render(request, 'blueprint.html', {})
+
+
+def calc(request):
+	return render(request, 'calc.html', {})
+
+
+def mystyle(request):
+	return render(request, 'mystyle.html', {})
